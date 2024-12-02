eb5db696212f40577404426cbdec97082bdbc9e2
Anindya-ctrl <and_@tutanota.com>
2024-12-02 21:33:39 +0600

    [2024] day 1 (part 2, didn't notice it existed)

--- 2024/day 1/main.cpp
+++ 2024/day 1/main.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 typedef long long ll;
 
+ll solve_part1(vector<ll> &a, vector<ll> &b) {
+  ll res = 0;
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+  for (ll i = 0; i < a.size(); ++i) {
+    res += abs(b[i] - a[i]);
+  }
+
+  return res;
+}
+
+ll solve_part2(vector<ll> &a, vector<ll> &b) {
+  ll res = 0, factor;
+  for (ll i = 0; i < a.size(); ++i) {
+    factor = 0;
+    for (ll j = 0; j < b.size(); ++j) {
+      if (a[i] == b[j])
+        factor++;
+    }
+
+    res += factor * a[i];
+  }
+
+  return res;
+}
+
 int main() {
   ifstream input("input.txt");
   if (!input) {
@@ -10,18 +37,14 @@ int main() {
   }
 
   vector<ll> a, b;
-  ll n1, n2, res = 0;
+  ll n1, n2;
   while (input >> n1 >> n2) {
     a.push_back(n1);
     b.push_back(n2);
   }
-  sort(a.begin(), a.end());
-  sort(b.begin(), b.end());
 
-  for (ll i = 0; i < a.size(); ++i) {
-    res += abs(b[i] - a[i]);
-  }
-  cout << res << "\n";
+  cout << solve_part1(a, b) << " ";
+  cout << solve_part2(a, b) << "\n";
 
   input.close();
   return 0;
